cdd2d4298dcbdd5ebb9bd6433cd20ae426514ff5
Saplonily <Saplonily@outlook.com>
2025-12-03 22:15:25 +0800

    full chat & player list

--- ChatInputBox/ChatMessageListView.cs
+++ ChatInputBox/ChatMessageListView.cs
@@ -1,40 +1,90 @@
 namespace Celeste.Mod.ChatInputBox;
 
-public sealed class ChatMessageListView
+// TODO support scrolling
+public sealed class ChatMessageListView<TChatMessage> where TChatMessage : IChatMessage
 {
+    private record struct ChatItem(TChatMessage Message, float ShowTimer, float FadeOut = 1f);
+    private readonly List<ChatItem> chatLog;
     private readonly ITextRenderer textRenderer;
+
+    public int MaxCount { get; set; } = 12;
+
+    public float ShowDuration { get; set; } = 2f;
+
+    public bool AlwaysShow { get; set; }
 
     public ChatMessageListView(ITextRenderer textRenderer)
     {
         this.textRenderer = textRenderer;
+        chatLog = new();
+    }
+
+    public void AddChatMessage(TChatMessage chatMessage)
+    {
+        chatLog.Add(new(chatMessage, ShowDuration));
+    }
+
+    public void CleanUp()
+    {
+        chatLog.Clear();
     }
 
     public void Update()
     {
-
+        for (int i = chatLog.Count - 1; i >= 0; i--)
+        {
+            var item = chatLog[i];
+            if (item.ShowTimer <= 0f)
+            {
+                if (item.FadeOut <= 0f)
+                {
+                    break;
+                }
+                else
+                {
+                    const float DisappearDuration = 0.25f;
+                    item.FadeOut -= (1f / DisappearDuration) * Engine.RawDeltaTime;
+                    if (item.FadeOut < 0f)
+                        item.FadeOut = 0f;
+                }
+            }
+            else
+            {
+                item.ShowTimer -= Engine.RawDeltaTime;
+            }
+            chatLog[i] = item;
+        }
     }
 
-    public void Render<TChatMessage>(IReadOnlyList<TChatMessage> messages)
-        where TChatMessage : IChatMessage
+    public void Render()
     {
+        //const float LinePadding = 2f;
         const float Margin = 16f;
         const float Padding = 8f;
 
         Vector2 baseLoc = new Vector2(Margin, Engine.Height - Margin - 64f);
 
-        float totalHeight = messages.Count * textRenderer.LineHeight;
-        float paddedHeight = totalHeight + 2 * Padding;
-
-        Draw.Rect(
-            baseLoc - Vector2.UnitY * paddedHeight,
-            Engine.Width / 3f * 2f + 2 * Padding,
-            paddedHeight,
-            Color.Black with { A = 100 }
-        );
-
         float curY = baseLoc.Y - Padding;
-        foreach (var msg in messages.Reverse())
+        foreach (var (msg, _, msgFade) in Enumerable.Reverse(chatLog).Take(MaxCount))
         {
+            float fade = msgFade;
+            if (AlwaysShow)
+            {
+                fade = 1f;
+            }
+            else if (fade <= 0f)
+                break;
+
+            float height = textRenderer.LineHeight;
+            float lineWidth = Engine.Width / 3f * 2f;
+            SnappedRect(
+                baseLoc.X - Padding,
+                curY - height,
+                lineWidth + 2 * Padding,
+                height,
+                ColorWithFade(Color.Black, fade * 0.5f)
+            );
+
             float curX = baseLoc.X + Padding;
             if (msg.Sender is not null)
             {
@@ -42,7 +92,7 @@ public sealed class ChatMessageListView
                     msg.Sender,
                     new Vector2(curX, curY),
                     new Vector2(0f, 1f),
-                    msg.SenderColor
+                    ColorWithFade(msg.SenderColor, fade)
                 );
                 curX += textRenderer.Measure(msg.Sender).X;
                 const string Separator = ": ";
@@ -50,7 +100,7 @@ public sealed class ChatMessageListView
                     Separator,
                     new Vector2(curX, curY),
                     new Vector2(0f, 1f),
-                    Color.White
+                    ColorWithFade(Color.White, fade)
                 );
                 curX += textRenderer.Measure(Separator).X;
             }
@@ -58,10 +108,23 @@ public sealed class ChatMessageListView
                 msg.Content,
                 new Vector2(curX, curY),
                 new Vector2(0f, 1f),
-                msg.Color
+                ColorWithFade(msg.Color, fade)
             );
 
             curY -= textRenderer.LineHeight;
+        }
+
+        static Color ColorWithFade(Color color, float fade)
+            => color with { A = (byte)(color.A * fade) };
+
+        static void SnappedRect(float x, float y, float width, float height, Color color)
+        {
+            float xi = MathF.Floor(x);
+            float yi = MathF.Floor(y);
+            float wi = MathF.Floor(x + width) - xi;
+            float hi = MathF.Floor(y + height) - yi;
+
+            Draw.Rect(xi, yi, wi, hi, color);
         }
     }
 }
